2efc512c43ba9fa282c050323149228a5a04d6e0
Sorin Antohi <sorin@sorinantohi.ro>
2021-05-04 23:16:47 +0300

    Decreased gas margin from 20 to 1%

--- WalletKitCore/src/ethereum/blockchain/BREthereumTransaction.h
+++ WalletKitCore/src/ethereum/blockchain/BREthereumTransaction.h
@@ -23,7 +23,7 @@ extern "C" {
 #define TRANSACTION_NONCE_IS_NOT_ASSIGNED   UINT64_MAX
 
 /// If we get a gasEstimate we'll want the gasLimit to have a margin over the estimate
-#define GAS_LIMIT_MARGIN_PERCENT        (20)
+#define GAS_LIMIT_MARGIN_PERCENT        (1)//(20)
 
 static inline BREthereumGas
 gasApplyLimitMargin (BREthereumGas gas) {
